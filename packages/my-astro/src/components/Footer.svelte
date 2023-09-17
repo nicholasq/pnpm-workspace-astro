b1948cab61b618032a09bebe3c1647c81da2f992
Nicholas Quezada <git.sharper162@passmail.net>
2023-09-17 00:38:55 -0700

    initial commit

---
+++ packages/my-astro/src/components/Footer.svelte
@@ -0,0 +1,2 @@
+<script lang="ts"></script>
+<footer>Hello svelte</footer>
---
+++ packages/my-astro/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<footer>Hello vue</footer>
+</template>
+
+<style scoped>
+
+</style>
